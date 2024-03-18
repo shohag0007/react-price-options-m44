af54a322abf49bffab6469e7b86160874a56e85e
shohag0007 <merinedrive007@gmail.com>
2024-03-18 22:25:45 +0600

    react navbar

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import NavBar from "./components/NavBar/NavBar"
+ import DaisyNav from "./components/DaisyNav/DaisyNav"
+
+function App() {
+
+
+  return (
+    <>
+     
+     
+
+     <NavBar></NavBar>
+     
+     <DaisyNav></DaisyNav>
+     
+     
+      <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
